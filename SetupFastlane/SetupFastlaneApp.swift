8f0a536db9e4778ece95badf4c5378cb83cee2c7
Hiteshmor <morhitesh@gmail.com>
2024-08-01 07:38:48 +0530

    Initial Commit

---
+++ SetupFastlane/SetupFastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetupFastlaneApp.swift
+//  SetupFastlane
+//
+//  Created by Hitesh Mor on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SetupFastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
